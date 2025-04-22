23dce24d09455125dcce7bb0d7990cc514f2dc11
Brendan Potter <84762913+brendanpotter00@users.noreply.github.com>
2025-04-21 19:38:01 -0500

    console log to dev mode

--- brendan-personal-website/src/sections/Landing.jsx
+++ brendan-personal-website/src/sections/Landing.jsx
@@ -6,37 +6,43 @@ import "../styles/landing.css";
 import "../styles/global.css";
 import { SHOW_PICTURES } from "../utils/globalVariables";
 import DebugOutline from "../components_jsx/DebugOutline";
+import { useAppContext } from "../context/AppContext";
 
-const Landing = () => (
-  <section id="landing" className={SHOW_PICTURES ? "picture-background" : ""}>
-    <Earth3D />
-    <div className="hero-wrapper">
-      <DebugOutline>
-        <h1 className="hero-heading">
-          <TypeAnimation
-            // Sequence: type, pause, then delete & repeat
-            sequence={[
-              "",
-              3000,
-              "Hello world",
-              2000, // type this, wait 2s
-              "Hello World!",
-              500,
-              "",
-              500, // delete it, wait 0.5s
-            ]}
-            speed={5} // typing speed in ms per character
-            deletionSpeed={10} // optional: speed at which it deletes
-            repeat={Infinity} // loop forever
-            cursor={true} // show the cursor
-            style={{ display: "inline-block" }}
-          />
-          <br />
-          My name is Brendan Potter
-        </h1>
-      </DebugOutline>
-    </div>
-  </section>
-);
+const Landing = () => {
+  const { developerMode } = useAppContext();
+  return (
+    <section id="landing" className={SHOW_PICTURES ? "picture-background" : ""}>
+      <Earth3D />
+      <div className="hero-wrapper">
+        <DebugOutline>
+          <h1 className="hero-heading">
+            {developerMode && "console.log("}
+            <TypeAnimation
+              // Sequence: type, pause, then delete & repeat
+              sequence={[
+                "",
+                3000,
+                "Hello world",
+                2000,
+                "Hello World!",
+                500,
+                "",
+                500,
+              ]}
+              speed={5}
+              deletionSpeed={10}
+              repeat={Infinity}
+              cursor={true}
+              style={{ display: "inline-block" }}
+            />
+            {developerMode && ");"}
+            <br />
+            My name is Brendan Potter
+          </h1>
+        </DebugOutline>
+      </div>
+    </section>
+  );
+};
 
 export default Landing;
